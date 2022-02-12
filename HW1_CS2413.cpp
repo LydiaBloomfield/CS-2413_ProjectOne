8a43a53dd59cbaca36e31b74d6e12176d7a12d54
LydiaBloomfield <lydia.r.bloomfield-1@ou.edu>
2022-02-12 15:58:22 -0600

    formatting changes and comments added

--- HW1_CS2413.cpp
+++ HW1_CS2413.cpp
@@ -7,17 +7,17 @@ protected:
 	int m; //The number of columns of the original matrix
 	int nonZeros; //The number of non-zero elements in the original matrix
 	int* values; //Array that contains all non-zero values of the matrix, assuming they are all integers
-	int valueIterator = 0;
+	int valueIterator = 0; // counter used in addValues()
 	int* rowPos; //Array that for each row of the original matrix contains the position in the values matrix in which the first non-zero element of this row is stored.
-	int rowIterator = 0;
+	int rowIterator = 0; // Counter used in add(Rows)
 	int* colPos; //Array that contains the column number for each one of the non-zero values in the original matrix.
-	int colIterator = 0;
-	int rowCur = 0;
-	int rowPrev = -1;
+	int colIterator = 0; // Counter used in addCol()
+	int rowCur = 0; // Used in addRow()
+	int rowPrev = -1; // Used in addRow()
 public:
 	CRM(); //default or empty constructor
-	CRM(int rows, int cols, int numNonZeros);
-	int getNumRows();
+	CRM(int rows, int cols, int numNonZeros); // constructor
+	int getNumRows(); //returns number of rows in original matrix
 	void addValue(int value); //add a new value in the values array
 	void addRow(int row); //add a row number in the rowPos array
 	void addColumn(int col);//add a column number in the colPos array
@@ -27,11 +27,11 @@ public:
 	int mostActiveUser(); //find the most active user
 	int* influentiality(); // array of each users influentiality (unranked)
 	int* activity(); // array of each users activity (unranked)
-	int* influentialUsers(); //compute vector of users ranked by how much influential they are
-	int* activeUsers(); //compute vector of users ranked by how much active they are
-	~CRM(); //destructor
-	//You may define and use any other method you need		
+	int* influentialUsers(); //compute vector of users ranked by how influential they are
+	int* activeUsers(); //compute vector of users ranked by how active they are
+	~CRM(); //destructor	
 };
+
 //some of the methods – Katia Papakonstantinopoulou
 CRM::CRM() {
 	n = 0;
@@ -54,30 +54,28 @@ int CRM::getNumRows() {
 	return n;
 }
 
-void CRM::addValue(int value)
-{
+void CRM::addValue(int value){
+	// add value to value array
 	values[valueIterator++] = value;
 }
 
-void CRM::addRow(int row)
-{
+void CRM::addRow(int row){
 	rowCur = row;
 
-	// if we encounter a user with no retweets, put -1 in rowPos 
+	// if we encounter a user who has not retweeted anyone, put -1 in rowPos 
 	while (rowCur > rowPrev + 1) {
 		rowPos[rowIterator++] = -1;
 		rowPrev++;
 	}
 
-	// if the current row is equal to previous row plus one, place the value found in the values array in the rowPos array
+	// if the current row is equal to previous row plus one, place the index of the values array in the rowPos array
 	if (rowCur == (rowPrev + 1)) {
 		rowPos[rowIterator++] = valueIterator - 1;
 	}
 
-	// if you've gotten to the end of the values array, and there are a bunch of users with no retweets at the end,
-	//add in -1 to the end of the rowPos array until the row pos array reaches its end
+	// if you reach the end of the values array, and there is still space in the rowPos array,
+	// add in -1 to the array until the row pos array reaches its end
 	if (valueIterator == nonZeros) {
-
 		while (rowIterator < n) {
 			rowPos[rowIterator++] = -1;
 		}
@@ -85,13 +83,13 @@ void CRM::addRow(int row)
 	rowPrev = rowCur;
 }
 
-void CRM::addColumn(int col)
-{
+void CRM::addColumn(int col){
+	// add column position
 	colPos[colIterator++] = col;
 }
 
-void CRM::display()
-{
+void CRM::display(){
+	// display arrays
 	cout << "values: ";
 	for (int i = 0; i < nonZeros; i++) {
 		cout << values[i] << " ";
@@ -106,64 +104,74 @@ void CRM::display()
 	}
 }
 
-int CRM::mostInfluentialUser() {
+int CRM::mostInfluentialUser(){
+	// return the first position of the mostInfluentialUsers array
 	return influentialUsers()[0];
 }
 
 int CRM::mostActiveUser(){
+	// return the first position of the mostActiveUsers array
 	return activeUsers()[0];
 }
 
-// Helper method
-int* CRM::influentiality()
-{
+// helper method to calculate influentiality of each user
+int* CRM::influentiality(){
 	int* outputVector = new int[n];
 	for (int i = 0; i < n; i++) outputVector[i] = 0;
 
-	// index of the output vector
+	// iterate throught the output vector with index i
 	for (int i = 0; i < n; i++) {
-		// iterate through the colPos array
 		int total = 0;
+		
+		// iterate through the colPos array with index j
 		for (int j = 0; j < nonZeros; j++) {
-			// if the value in the colPos array is the same as the column of the current user, add it to the user's total
+			// if the value in the colPos array is the same as the id of the current user, add it to the user's total
 			if (colPos[j] == i) {
 				total += values[j];
 			}
 		}
+		// set the current user's total
 		outputVector[i] = total;
 	}
 	return outputVector;
 }
 
-int* CRM::activity()
-{
+// helper method to calculate activity of each user
+int* CRM::activity(){
 	int* outputVector = new int[n];
 	for (int i = 0; i < n; i++) outputVector[i] = 0;
+	
 	int indexOfValues = 0;
 
-	// index of output vector
+	// iterate through the output vector with index i
 	for (int i = 0; i < n; i++) {
 		int total = 0;
 
-		//Iterate through values array
+		// if the next value in the rowPos array is -1
 		if (rowPos[i + 1] == -1) {
+			// then add all values until the next value that does not equal -1
 			while (indexOfValues < rowPos[i + 2]) {
 				total += values[indexOfValues];
 				indexOfValues++;
 			}
+			// the activity total of the current user is total, and the activity total of the next user is 0
 			outputVector[i + 1] = 0;
 			outputVector[i] = total;
 			i++;
 		}
-		// last element of array
+
+		// if we are at the end of the array
 		else if (i == n - 1) {
+			// then add up all the remaining values in the values array
 			while (indexOfValues < nonZeros) {
 				total += values[indexOfValues];
 				indexOfValues++;
 			}
 			outputVector[i] = total;
 		}
+		
 		else {
+			// add up all the values in the values array until the index is equal to the next value in the rowPos array
 			while (indexOfValues < rowPos[i + 1]) {
 				total += values[indexOfValues];
 				indexOfValues++;
@@ -179,10 +187,10 @@ int* CRM::influentialUsers() {
 	int* outputVector = new int[n];
 	for (int i = 0; i < n; i++) outputVector[i] = i;
 
-	int ciTemp;
-	int i, j, temp;
+	int ciTemp, i, j, temp;
 
 	// selection sort
+	// as we sort the array from influentiality(), we simultaneously sort an array of user ids (outputVector)
 	for (i = 0; i < n; i++) {
 		for (j = i + 1; j < n; j++) {
 			if (copy[i] < copy[j]) {
@@ -206,13 +214,14 @@ int* CRM::activeUsers()
 	int* outputVector = new int[n];
 	for (int i = 0; i < n; i++) outputVector[i] = i;
 
-	int ciTemp;
-	int i, j, temp;
+	int ciTemp, i, j, temp;
 
 	// selection sort
+	// as we sort the array from activity(), we simultaneously sort an array of user ids (outputVector)
 	for (i = 0; i < n; i++) {
 		for (j = i + 1; j < n; j++) {
 
+			// if total activity of the current user is less than the next, switch their order
 			if (copy[i] < copy[j]) {
 				temp = copy[i];
 				ciTemp = outputVector[i];
@@ -223,9 +232,10 @@ int* CRM::activeUsers()
 				copy[j] = temp;
 				outputVector[j] = ciTemp;
 			}
-			//if the values are equal, 
+
+			//if the total activity of the current and next users are equal, 
 			if (copy[i] == copy[j]) {
-				// then the earlier position is given to the one with the lower id
+				// then the earlier position is given to the user with the lower id
 				if (outputVector[j] < outputVector[i]) {
 					ciTemp = outputVector[i];
 					outputVector[i] = outputVector[j];
@@ -264,12 +274,11 @@ int main() {
 
 	A = new CRM(numRows, numColumns, numNonZeros);
 
-	// put in numNonZeros as number of iterations of this loop
 	for (int i = 0; i < numNonZeros; i++) {
-		// read in row of information
+		// read in row of information and add to arrays
 		cin >> row >> col >> value;
 		(*A).addValue(value);
-		(*A).addRow(row);//needs to be done cleverly in the method
+		(*A).addRow(row);
 		(*A).addColumn(col);
 	}
 
@@ -287,15 +296,14 @@ int main() {
 	cout << "Most active user: " << mostAct << endl;
 	cout << endl;
 
-	// Rank users based on how much influential they are
+	// Rank users based on how influential they are
 	int* influentialityVector = (*A).influentialUsers();
 	cout << "Users ranked by Influentiality: ";
 	for (int i = 0; i < (*A).getNumRows(); i++)
 		cout << influentialityVector[i] << " ";
 	cout << endl << endl;
 
-	//Rank users based on how much active they are
-	//fill-in code
+	//Rank users based on how active they are
 	int* activityVector = (*A).activeUsers();
 	cout << "Users ranked by Activity: ";
 	for (int i = 0; i < (*A).getNumRows(); i++)
